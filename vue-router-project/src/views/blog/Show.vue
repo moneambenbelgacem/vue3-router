83826fc6fe4b5e996728ffddd42c3a6027a72e5f
snowden96 <moneem.benbelgasem@esen.tn>
2021-01-23 14:11:45 +0100

    passer les props en route

--- vue-router-project/src/views/blog/Show.vue
+++ vue-router-project/src/views/blog/Show.vue
@@ -1,17 +1,13 @@
 <template lang="">
   <div>
     <h1>show post</h1>
-    <h2>id :{{ id }}</h2>
+    <h2>id :{{ id }} == slug : {{ slug }}</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit.</p>
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    };
-  },
+  props: ['id', 'slug'],
 };
 </script>
 <style lang=""></style>
