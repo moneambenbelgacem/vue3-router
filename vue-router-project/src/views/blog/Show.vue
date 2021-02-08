61dd4a68f49bcbb79036d587f012270838de7bf5
snowden96 <moneem.benbelgasem@esen.tn>
2021-02-08 15:51:18 +0100

    connecter le composant show a la data json

--- vue-router-project/src/views/blog/Show.vue
+++ vue-router-project/src/views/blog/Show.vue
@@ -1,13 +1,23 @@
 <template lang="">
   <div>
-    <h1>show post</h1>
-    <h2>id :{{ id }} == slug : {{ slug }}</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit.</p>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.content }}</p>
   </div>
 </template>
 <script>
 export default {
   props: ['id', 'slug'],
+  data() {
+    return {
+      post: {},
+    };
+  },
+  mounted() {
+    fetch(`http://localhost:5000/posts/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.post = data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 <style lang=""></style>
