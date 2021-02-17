8f38cba9ea607f1a3ca016c0972795f6a3f8e602
snowden96 <moneem.benbelgasem@esen.tn>
2021-02-17 15:11:55 +0100

    reglement le fetching data f show avec v-if et v-else

--- vue-router-project/src/views/blog/Show.vue
+++ vue-router-project/src/views/blog/Show.vue
@@ -1,22 +1,25 @@
 <template lang="">
-  <div>
+  <div v-if="post">
     <h1>{{ post.title }}</h1>
     <p>{{ post.content }}</p>
   </div>
+  <div v-else class="text-centre">Loding...</div>
 </template>
 <script>
 export default {
   props: ['id', 'slug'],
   data() {
     return {
-      post: {},
+      post: null,
     };
   },
   mounted() {
-    fetch(`http://localhost:5000/posts/${this.id}`)
-      .then((res) => res.json())
-      .then((data) => (this.post = data))
-      .catch((err) => console.log(err));
+    setTimeout(() => {
+      fetch(`http://localhost:5000/posts/${this.id}`)
+        .then((res) => res.json())
+        .then((data) => (this.post = data))
+        .catch((err) => console.log(err));
+    }, 3000);
   },
 };
 </script>
